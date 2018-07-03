29a07dc66ba888dfe5a3c90e13ad93df4b547291
Julian Wagner <jwagner2@caltech.edu>
2018-07-03 11:53:51 -0700

    Progress on segmentation functions, added toy data for testing

--- behavioral_analysis/segmentation.py
+++ behavioral_analysis/segmentation.py
@@ -8,20 +8,82 @@ def _check_image_input(im):
     if not len(im.shape) == 2:
         raise RuntimeError("Need to provide an array with shape (n, m). Provided array has shape " + str(im.shape))
 
-def _check_function_input(im, thresh_func, args=()):
+def _check_numeric_types(number):
+    if (isinstance(number, float)       or
+        isinstance(number, int)         or
+        isinstance(number, np.int)      or
+        isinstance(number, np.int32)    or
+        isinstance(number, np.int64)    or
+        isinstance(number, np.float)    or
+        isinstance(number, np.float16)  or
+        isinstance(number, np.float32)  or
+        isinstance(number, np.float64)  or
+        isinstance(number, np.float128) ):
+        return True
+    return False
+        
+def _check_function_input(im, thresh_func, args):
     
     if not callable(thresh_func):
         raise RuntimeError("The provided function is not callable")
         
-    if not isinstance(thresh_func(im, args), float):
-        raise RuntimeError("The provided function does not output float, gives " + str(type(thresh_func(im, args))))
-        
+    func_out = thresh_func(im, *args)
+    if not (_check_numeric_types(func_out) or isinstance(func_out, np.ndarray)):
+        raise RuntimeError("The provided function must output a numeric or array \
+                           provided function returns type " + str(type(func_out)))
+    if isinstance(func_out, np.ndarray) and not func_out.shape == im.shape:
+        raise RuntimeError("Array output of the function must have same shape as the image \
+                           the output array has shape " + str(func_out.shape) +
+                           ", image has shape " + str(im.shape))
+
     return True
 
-def segment(im, thresh_func=skimage.filters.threshold_otsu, thresh_approach='By value', args=()):
+def _check_ims_same_dim(im1, im2):
+    if not (im1.shape == im2.shape):
+        raise RuntimeError("The provided images have different dimension \
+        im1: "+str(im1.shape)+", im2: "+str(im2.shape))
+
+def bg_subtract(im1, im2):
+    """Function to perform background subtraction on an image
+    using a blank image of the arena of interest.
+    
+    Parameters
+    ----------
+    im1 : im, numpy.ndarray with shape (n, m) (with 0 < m, n)
+        The image (with only one color chanel) to subtract the
+        background from.
+    im2 : im, numpy.ndarray with shape (n, m) (with 0 < m, n)
+        The background image (with only one color chanel) to subtract the
+        from im1.
+    Returns
+    -------
+    output : 2d numpy.ndarray with shape (n, m)
+        image with background subtracted, i.e. im1-im2.
+    """
+    
+    _check_image_input(im1)
+    _check_image_input(im2)
+    _check_ims_same_dim(im1, im2)
+    
+    im1 = normalize_convert_im(im1)
+    im2 = normalize_convert_im(im2)
+    
+    im_no_bg = im1-im2
+    im_no_bg = normalize_convert_im(im_no_bg)
+    return (im_no_bg)
+
+def normalize_convert_im(im):
+    im = skimage.img_as_float(im)
+    im_norm = im - im.min()
+    im_norm = im / im.max()
+    return im
+
+def segment(im, thresh_func=skimage.filters.threshold_otsu, args=()):
     """Function to threshold an image using skimage functions. 
     The user pases the desired function to determine the threshold
-    for the data (or a value to use as the threshold).
+    for the data (or a value to use as the threshold). This value
+    for the threshold level can be a float/int or an array of same shape
+    as the input image.
     
     Parameters
     ----------
@@ -29,7 +91,7 @@ def segment(im, thresh_func=skimage.filters.threshold_otsu, thresh_approach='By 
         The image (with only one color chanel) to threshold.
     thresh_func : function
         The function to use to calculate the thresholding. Should
-        return a single scalar value.
+        return a single scalar value or a numpy array.
     Returns
     -------
     output : 2d numpy.ndarray with shape (n, m)
@@ -39,6 +101,12 @@ def segment(im, thresh_func=skimage.filters.threshold_otsu, thresh_approach='By 
     _check_image_input(im)
     _check_function_input(im, thresh_func, args)
     
-    thresh = thresh_func(im, args)
+    thresh = thresh_func(im, *args)
     
-    pass
+    im_bw = im < thresh
+    
+    # Label binary image; background kwarg says value in im_bw to be background
+    im_labeled, n_labels = skimage.measure.label(im_bw, background=0, return_num=True)
+    
+    return im_bw, im_labeled, n_labels
+    
--- tests/test_segmentation_pipeline.py
+++ tests/test_segmentation_pipeline.py
@@ -49,11 +49,47 @@ def test_provided_function_callable():
         segmentation.segment(im, thresh_func='Hello, world.')
     excinfo.match("The provided function is not callable")
     
-def test_provided_function_callable():
+def test_provided_function_callable_mat():
     im = np.array([[1, 2, 3], [1, 2, 3]])
-    args = (3)
-    assert segmentation._check_function_input(im, thresh_func=skimage.filters.threshold_local, *args) == True
+    args = (3,)
+    assert segmentation._check_function_input(im, skimage.filters.threshold_local, args) == True
 
-def test_provided_function_returns_float():
+def test_provided_function_returns_correct_shape():
+    im = np.array([[1, 2, 3], [1, 2, 3]])
+    def bad_func(im):
+        return(np.array([[1, 2], [1, 2]]))
+    with pytest.raises(RuntimeError) as excinfo:
+        segmentation.segment(im, thresh_func=bad_func)
+    excinfo.match("Array output of the function must have same shape as the image \
+                           the output array has shape \(2, 2\), image has shape \(2, 3\)")
+
+def test_provided_function_returns_correct_types():
+    im = np.array([[1, 2, 3], [1, 2, 3]])
+    def bad_func(im):
+        return('Hello, world!')
+    with pytest.raises(RuntimeError) as excinfo:
+        segmentation.segment(im, thresh_func=bad_func)
+    excinfo.match("The provided function must output a numeric or array \
+                           provided function returns type <class 'str'>")
     
-    pass
+def test_check_numeric_function():
+    assert segmentation._check_numeric_types(np.int32(1)) == True
+    
+def test_bg_subtract_im_type():
+    im1 = np.array([[1, 2, 3], [1, 2, 3]])
+    im2 = np.array([[[1, 2], [1, 2]], [[1, 2], [1, 2]]])
+    with pytest.raises(RuntimeError) as excinfo:
+        segmentation.bg_subtract(im1, im2)
+    excinfo.match("Need to provide an array with shape \(n, m\). Provided array has shape \(2, 2, 2\)")
+    
+def test_bg_subtract_im_dims():
+    im1 = np.array([[1, 2, 3], [1, 2, 3]])
+    im2 = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+    with pytest.raises(RuntimeError) as excinfo:
+        segmentation.bg_subtract(im1, im2)
+    excinfo.match("The provided images have different dimension \
+        im1: \(2, 3\), im2: \(3, 3\)")
+    
+    
+    
+ 
