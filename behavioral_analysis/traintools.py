ed8a7c2f4a8c6d5c2e7521c28b4c37e6ed5e71cc
Julian Wagner <jwagner2@caltech.edu>
2018-07-20 17:32:09 -0700

    Added documentation for segmentation and in notebook data labeling

--- behavioral_analysis/traintools.py
+++ behavioral_analysis/traintools.py
@@ -7,7 +7,7 @@ from bokeh.models import ColumnDataSource, Slider, Button
 from bokeh.themes import Theme
 from bokeh.io import show
 from bokeh.plotting import figure, output_file, Column
-from bokeh.models import DataTable, TableColumn, PointDrawTool, ColumnDataSource, CrosshairTool
+from bokeh.models import DataTable, TableColumn, PointDrawTool, ColumnDataSource, CrosshairTool, CDSView, BooleanFilter
 from bokeh.events import DoubleTap
 from bokeh.models.widgets import TextInput
 from bokeh.palettes import viridis
@@ -18,12 +18,10 @@ import bootcamp_utils
 def _check_ims(ims):
     if not segmentation._check_array_like(ims):
         raise RuntimeError("The given ims object is not array like, it is " + str(type(ims)))
-    #if not len(np.array(ims).shape) == 3:
-    #    raise RuntimeError("Need to provide an array with shape (n, m, p). Provided array has shape " +
-    #                       str(np.array(ims).shape))
     [segmentation._check_image_input(im) for im in ims]
     
-def point_label(ims):
+def point_label(ims, point_size=3, table_height=200, crosshair_tool_alpha=0.5,
+                point_tool_color='white'):
         
     _check_ims(ims)
     ims = np.array(ims)
@@ -31,68 +29,71 @@ def point_label(ims):
     max_height = max(np.array([b.shape for b in ims])[:, 0])
     max_width = max(np.array([b.shape for b in ims])[:, 1])
 
-    point_labels = ColumnDataSource({'x': [],
-                                     'y': [],
-                                     'frame': []})
-    
-    alphas = []
-
-    pal = viridis(len(ims))
+    point_labels = ColumnDataSource({'x': [], 'y': [], 'frame': []})
 
     def modify_doc(doc):
         im_num = [0,]
 
         images = [np.pad(im, ((max_height-im.shape[0], 0), (0, max_width-im.shape[1])), 'constant') for im in ims]
-        
+
         plot, source = bootcamp_utils.viz.bokeh_imshow(images[im_num[-1]], return_im=True)
         source = source.data_source
 
-        renderer = plot.scatter(x='x', y='y', source=point_labels, color='white', size=3)
+        booleans = [True if frame == im_num[-1] else False for frame in point_labels.data['frame']]
+        view = CDSView(source=point_labels, filters=[BooleanFilter(booleans)])
+
+        renderer = plot.scatter(x='x', y='y', source=point_labels, view=view,
+                                color=point_tool_color, size=point_size)
         columns = [TableColumn(field="x", title="x"),
                    TableColumn(field="y", title="y"),
                    TableColumn(field='frame', title='frame')]
-        table = DataTable(source=point_labels, columns=columns, editable=True, height=200)
+        table = DataTable(source=point_labels, columns=columns, editable=True, height=table_height)
         draw_tool = PointDrawTool(renderers=[renderer], empty_value=im_num[-1])
         plot.add_tools(draw_tool)
-        plot.add_tools(CrosshairTool(line_alpha=0.5))
+        plot.add_tools(CrosshairTool(line_alpha=crosshair_tool_alpha))
         plot.toolbar.active_tap = draw_tool
+        
+        def update_image(new_ind):
+            _, data = bootcamp_utils.viz.bokeh_imshow(images[new_ind], return_im=True)
+            data = data.data_source
+            source.data = data.data
+        
+        def callback_point_view(event):
+            booleans = [True if frame == im_num[-1] else False for frame in point_labels.data['frame']]
+            view = CDSView(source=point_labels, filters=[BooleanFilter(booleans)])
+            renderer.view = view
 
-        def callback(attr, old, new):
+        def callback_slider(attr, old, new):
+            update_image(new)
+            
             im_num.append(int(new))
-            _, data = bootcamp_utils.viz.bokeh_imshow(images[int(new)], return_im=True)
-            data = data.data_source
-            source.data = data.data
             draw_tool.empty_value = im_num[-1]
+            callback_point_view('tap')
 
-        def callback_forward():
-            if (len(images) - 2) < im_num[-1]:
+        def callback_button(direction):
+            new = im_num[-1]+direction
+            if (((len(images) - 1) < new and direction == 1) or
+                (new == -1 and direction == -1)):
                 return None
-            _, data = bootcamp_utils.viz.bokeh_imshow(images[im_num[-1]+1], return_im=True)
-            im_num.append(im_num[-1]+1)
-            data = data.data_source
-            source.data = data.data
+            update_image(new)
+            
+            im_num.append(new)
             draw_tool.empty_value = im_num[-1]
-
-        def callback_backward():
-            if im_num[-1] == 0:
-                return None
-            _, data = bootcamp_utils.viz.bokeh_imshow(images[im_num[-1]-1], return_im=True)
-            im_num.append(im_num[-1]-1)
-            data = data.data_source
-            source.data = data.data
-            draw_tool.empty_value = im_num[-1]
-
-        slider = Slider(start=0, end=29, value=0, step=1, title="Frame Number")
+            callback_point_view('tap')
+            
+        slider = Slider(start=0, end=len(images), value=0, step=1, title="Frame Number")
+        slider.on_change('value', callback_slider)
+        
         button_back = Button(label='back',button_type="success")
+        button_back.on_click(partial(callback_button, direction=-1))
+
         button_forward = Button(label='forward',button_type="success")
-        #text_input = TextInput(value="0", title="Frame Num:")
-        #text_input.on_change('value', callback)
-        slider.on_change('value', callback)
-        button_forward.on_click(callback_forward)
-        button_back.on_click(callback_backward)
+        button_forward.on_click(partial(callback_button, direction=1))
+        
+        plot.on_event('tap', callback_point_view)
 
         doc.add_root(column(row(slider), plot, row(button_back, button_forward), table))
-    
+
     show(modify_doc)
     return point_labels
 
@@ -128,21 +129,16 @@ def button_label(ims, button_values=('beetle', 'ant')):
             #plot.plot_width = temp_plot.plot_width
             #layout.children[1] = plot
 
-        def callback_forward():
-            if (len(images) - 2) < im_num[-1]:
+        def callback_button(direction):
+            if (((len(images) - 2) < im_num[-1] and direction == 1) or
+                (im_num[-1] == 0 and direction == -1)):
                 return None
-            _, data = bootcamp_utils.viz.bokeh_imshow(images[im_num[-1]+1], return_im=True)
-            im_num.append(im_num[-1]+1)
+            _, data = bootcamp_utils.viz.bokeh_imshow(images[im_num[-1]+direction], return_im=True)
+            im_num.append(im_num[-1]+direction)
             data = data.data_source
             source.data = data.data
-
-        def callback_backward():
-            if im_num[-1] == 0:
-                return None
-            _, data = bootcamp_utils.viz.bokeh_imshow(images[im_num[-1]-1], return_im=True)
-            im_num.append(im_num[-1]-1)
-            data = data.data_source
-            source.data = data.data
+            draw_tool.empty_value = im_num[-1]
+            callback_point_view('tap')
 
         def callback_label_button(value):        
             new_data = {'type': [value],
@@ -157,11 +153,13 @@ def button_label(ims, button_values=('beetle', 'ant')):
             source.data = data.data
 
         slider = Slider(start=0, end=len(images)-1, value=0, step=1, title="Frame Number")
-        button_back = Button(label='back',button_type="success")
-        button_forward = Button(label='forward',button_type="success")
         slider.on_change('value', callback)
-        button_forward.on_click(callback_forward)
-        button_back.on_click(callback_backward)
+        
+        button_back = Button(label='back',button_type="success")
+        button_back.on_click(partial(callback_button, direction=-1))
+
+        button_forward = Button(label='forward',button_type="success")
+        button_forward.on_click(partial(callback_button, direction=1))
 
         label_buttons = [Button(label=value, button_type='success') for value in button_values]
         [button.on_click(partial(callback_label_button, value=value)) for button, value in zip(label_buttons, button_values)]
